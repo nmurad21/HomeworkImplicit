773235254ae24fa392c92630934287bd5e3b4a2c
User <User@WIN-CUEGFOLCMBN>
2022-03-25 23:49:30 +0400

    Add project files.

---
+++ HomeworkImplicit/Models/Enum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkImplicit.Models
+{
+   enum Currecy
+    {
+        Eur,
+        Usd,
+        Try
+    }
+}
---
+++ HomeworkImplicit/Program.cs
@@ -0,0 +1,31 @@
+﻿using HomeworkImplicit.Models;
+using System;
+
+namespace HomeworkImplicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Exchange(Currecy currecy, double azn)
+            {
+                double result = 0;
+                if (currecy == Models.Currecy.Eur)
+                {
+                    result = azn * 0.49;
+                }
+                if (currecy == Models.Currecy.Usd)
+                {
+                    result = azn * 0.59;
+                }
+                if (currecy == Models.Currecy.Try)
+                {
+                    result = azn * 8.7;
+                }
+
+                return result;
+            }
+            Console.WriteLine(Exchange(Currecy.Try,45));
+        }
+    }
+}
